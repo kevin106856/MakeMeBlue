2ee4518ba242ff1e3d9e3724afad121a04dfc851
ARTEAGA,KA KEVIN <rl106856@rlas-macair-20.rlas-116.org>
2018-12-12 11:00:50 -0600

    Initial Commit

---
+++ MakeMeBlue/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MakeMeBlue
+//
+//  Created by ARTEAGA,KA KEVIN on 8/31/18.
+//  Copyright © 2018 ARTEAGA,KA KEVIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var makeMeBlueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeMeBlueLabel.textColor = .blue
+    }
+
+   
+
+
+}
+
